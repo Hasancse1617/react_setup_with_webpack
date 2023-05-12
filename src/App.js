a1bde1effed6bb37714bbcba4b4dd4d0b9d49b48
Hasan Ali <hasancseiu16@gmail.com>
2023-05-12 12:31:24 +0600

    added image support

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import "./App.css";
+import Image from "./hasan.jpg";
 
 function App() {
   return (
-    <div>Hello Hasan Ali</div>
+    <>
+       <div>Hello Hasan Ali</div>
+       <div><img src={Image}></img></div>
+    </>
   )
 }
 export default App;
